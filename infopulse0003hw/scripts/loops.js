ff780e04e712d5d4600c23a8d113f59644b1dab6
osama <alsamancov@gmail.com>
2018-04-24 18:52:01 +0300

    "commit from work 24042018"

---
+++ infopulse0003hw/scripts/loops.js
@@ -0,0 +1,10 @@
+var array = [11, 5, 2, 7, 4, 1, 9, 6, 3, 0, 10, 8];
+
+
+var sum = 0;
+
+for(var i = 0; i < array.length; i++){
+    sum += array[i];
+}
+
+console.log("The sum of all the numbers in the array: ", sum);
